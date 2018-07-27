e50a845c8c2ce0f075f5c565ecc069d2193944d4
ujin43255252 <ujin43255252@gmail.com>
2018-07-27 11:53:36 +0900

    scss Sizes Added & FormWrapper Added & Create Page AlmostDone

---
+++ src/components/FormWrapper.vue
@@ -0,0 +1,61 @@
+<template lang=''>
+  <div class="formWrapper" :class="margin && 'marginTop'">
+    <span class="formTitle">
+      {{title}}
+    </span>
+    <input v-if="!textarea" :type="type" @change="$emit('update:content', input)" v-model="input" :placeholder="placeholder" />
+    <textarea maxlength="1500" v-model="input" class="textArea" @change="$emit('update:content', input)" :placeholder="placeholder" v-else />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    placeholder: String,
+    content: String,
+    type: String,
+    textarea: Boolean,
+    margin: Boolean
+  },
+  data () {
+    return {
+      input: ""
+    }
+  },
+  created () {
+    this.input = this.content;
+  }
+}
+</script>
+<style lang='scss'>
+.formWrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .formTitle {
+    font-size: $h1-font-size;
+    font-weight: $big-font-weight;
+    margin-bottom: 12px;
+  }
+  .textArea {
+    @include boxBorder(yes);
+    width: 100%;
+    padding: 8px 10px;
+    font-size: $normal-font-size;
+    font-weight: $small-font-weight;
+    resize: none;
+    outline: none;
+    height: 400px;
+  }
+  input {
+    @include boxBorder(yes);
+    width: 100%;
+    padding: 8px 10px;
+    font-size: $normal-font-size;
+    outline: none;
+  }
+}
+.marginTop {
+  margin-top: 35px;
+}
+</style>
--- src/pages/CreatePaper.vue
+++ src/pages/CreatePaper.vue
@@ -14,16 +14,37 @@
     </div>
   </div>
   <div class="row">
-
+    <div class="column">
+      <form-wrapper type="text" :margin="true" title="제목" placeholder="제목을 입력하세요." :content.sync="title" ></form-wrapper>
+      <form-wrapper type="datetime-local" :margin="true" title="날짜 기한(Deadline)" placeholder="제목을 입력하세요." :content.sync="time" ></form-wrapper>
+      <form-wrapper type="text" :textarea="true" :margin="true" title="페이퍼 설명(1500자 이내)" placeholder="이 어플라이에 대한 설명을 입력해주세요." :content.sync="explaination" ></form-wrapper>
+    </div>
+    <div class="column">
+      <form-wrapper type="text" :margin="true" title="URL(자보, 아라)" placeholder="ex) https://zabo.sparcs.org/zabo/24" :content.sync="url" ></form-wrapper>
+      <mini-view></mini-view>
+    </div>
   </div>
 </div>
 </template>
 <script>
+import FormWrapper from '@/components/FormWrapper';
+import MiniView from '@/components/MiniView.vue';
+
 export default {
   data () {
     return {
-      selectedCategory: ""
+      selectedCategory: "",
+      title: "",
+      explaination: "",
+      time: "",
+      url: ""
     }
+  },
+  components: {
+    FormWrapper,
+    MiniView
+  },
+  methods: {
   }
 }
 </script>
@@ -40,35 +61,50 @@ export default {
     display: flex;
     justify-content: flex-start;
     align-items: flex-start;
-    .headingWrapper {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      .headingTitle {
-        font-size: $big-font-size;
-        font-weight: $big-font-weight;
-      }
-      .essential {
-        font-size: $normal-font-size;
-        color: $essential-color;
-        margin-left: 5px;
-      }
-    }
-    .categoryWrapper {
-      margin-left: 40px;
-      .categoryHeading {
-        font-size: $normal-font-size;
-        font-weight: $big-font-weight;
-      }
-      .radio {
-        margin-left: 15px;
-      }
-      .label {
-        font-size: $normal-font-size;
-      }
-    }
     &:first-child {
       align-items: center;
+      .headingWrapper {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        .headingTitle {
+          font-size: $big-font-size;
+          font-weight: $big-font-weight;
+        }
+        .essential {
+          font-size: $normal-font-size;
+          color: $essential-color;
+          margin-left: 5px;
+        }
+      }
+      .categoryWrapper {
+        margin-left: 40px;
+        .categoryHeading {
+          font-size: $normal-font-size;
+          font-weight: $big-font-weight;
+        }
+        .radio {
+          margin-left: 15px;
+        }
+        .label {
+          font-size: $normal-font-size;
+        }
+      }
+    }
+    &:last-child {
+      .column {
+        &:first-child {
+          flex: 3;
+          margin-right: 80px;
+        }
+        &:last-child {
+          flex: 2;
+        }
+        .formWrapper {
+          .formTitle {
+          }
+        }
+      }
     }
   }
 }
