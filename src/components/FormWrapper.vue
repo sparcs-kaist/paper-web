b39cbdc410bcc175d75870d61b6773c34a66d5ea
ujin43255252 <ujin43255252@gmail.com>
2018-07-27 13:38:33 +0900

    FormWrapper created & global mixin / colors Scss Added

--- src/components/FormWrapper.vue
+++ src/components/FormWrapper.vue
@@ -1,9 +1,10 @@
 <template lang=''>
   <div class="formWrapper" :class="margin && 'marginTop'">
-    <span class="formTitle">
-      {{title}}
-    </span>
-    <input v-if="!textarea" :type="type" @change="$emit('update:content', input)" v-model="input" :placeholder="placeholder" />
+    <div class="formTitleWrapper">
+      <span class="formTitle">{{title}}<span class="requiredSpan" v-show="required">*</span></span>
+        <v-switch @click.native="clearInput" v-if="toggle" color="success" style="padding: 0;margin-top: 0px; max-height: 20px; " label="포함하기" v-model="toggleState"></v-switch>
+    </div>
+    <input :disabled="!toggleState" v-if="!textarea" :type="type" @change="$emit('update:content', input)" v-model="input" :placeholder="placeholder" />
     <textarea maxlength="1500" v-model="input" class="textArea" @change="$emit('update:content', input)" :placeholder="placeholder" v-else />
   </div>
 </template>
@@ -15,27 +16,50 @@ export default {
     content: String,
     type: String,
     textarea: Boolean,
-    margin: Boolean
+    margin: Boolean,
+    required: Boolean,
+    toggle: Boolean,
   },
   data () {
     return {
-      input: ""
+      input: "",
+      toggleState: false
     }
   },
   created () {
     this.input = this.content;
+  },
+  methods: {
+    clearInput () {
+      if (!this.toggleState) {
+        this.input = "";
+        this.$emit('update:content', this.input)
+      }
+    }
   }
 }
 </script>
-<style lang='scss'>
+<style lang='scss' scoped>
 .formWrapper {
   width: 100%;
   display: flex;
   flex-direction: column;
-  .formTitle {
+  .formTitleWrapper {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    margin-bottom: 12px;
+    .formTitle {
     font-size: $h1-font-size;
     font-weight: $big-font-weight;
-    margin-bottom: 12px;
+    min-width: 180px;
+    }
+    .requiredSpan {
+      margin-left: 10px;
+      margin-right: 20px;
+      font-size: $h2-font-size;
+      color: $essential-color;
+    }
   }
   .textArea {
     @include boxBorder(yes);
