470c2c9d0c38f2b0366d46534a91ae827f849aed
ujin43255252 <ujin43255252@gmail.com>
2018-07-31 20:31:41 +0900

    Proxy & Axios url Added

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://138.91.120.134:40400/"
+  baseURL: "http://ssal.sparcs.org:16138"
 });
 
 instance.interceptors.request.use(req => req, error => Promise.reject(error));
