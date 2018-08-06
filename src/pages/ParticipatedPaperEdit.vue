85d4db50e1f6ddd753fee37ccb533328b2f28c8f
ujin43255252 <ujin43255252@gmail.com>
2018-08-06 22:56:22 +0900

    Participated - Submitted/Editted Page css & routing Added

--- src/pages/ParticipatedPaperEdit.vue
+++ src/pages/ParticipatedPaperEdit.vue
@@ -1,14 +1,25 @@
 <template lang=''>
   <div class="totalWrapper">
-    <span class="replyWrapper">페이퍼를 성공적으로 수정하셨습니다.</span>
+    <span class="replyWrapper">답변을 성공적으로 수정하셨습니다.</span>
     <div class="tabWrapper">
-      <span class="tabs">메인으로 돌아가기</span>
-      <span class="tabs">마이페이지 가기</span>
+      <span @click="routing('Main')" class="tabs">메인으로 돌아가기</span>
+      <span @click="routing('MyPage')" class="tabs">마이페이지 가기</span>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    routing(str) {
+      if (str == "Main") {
+        this.$router.push({ name: "MainPage" });
+      }
+      if (str == "MyPage") {
+        this.$router.push({ name: "MyPage" });
+      }
+    }
+  }
+};
 </script>
 <style scoped lang='scss'>
 .totalWrapper {
@@ -22,6 +33,11 @@ export default {};
     font-weight: $big-font-weight;
     text-align: center;
   }
+  .urlWrapper {
+    margin-top: 20px;
+    font-size: $normal-font-size;
+    text-align: center;
+  }
   .tabWrapper {
     width: 100%;
     display: flex;
@@ -29,6 +45,9 @@ export default {};
     .tabs {
       flex: 1;
       font-size: $big-font-size;
+      color: $font-black-light;
+      transition: all 0.3s ease-in-out;
+      cursor: pointer;
       &:first-child {
         text-align: right;
         padding-right: 10px;
@@ -36,6 +55,9 @@ export default {};
       &:last-child {
         text-align: left;
         padding-left: 10px;
+      }
+      &:hover {
+        color: $font-black-dark;
       }
     }
   }
