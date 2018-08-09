a3a9559acc5c9295cb8158ffb4923f362a87ff45
ujin43255252 <ujin43255252@gmail.com>
2018-08-09 23:36:50 +0900

    SPARCS SSO Login Function Adding

---
+++ src/pages/LoginCallback.vue
@@ -0,0 +1,32 @@
+<template lang=''>
+<div>
+  Logging In...
+</div>
+</template>
+<script>
+import axios from "@/axios-auth";
+
+export default {
+  created() {
+    console.log(this.$route.params.email);
+    if (localStorage.getItem("token")) {
+      this.$router.push({ name: "MainPage" });
+    } else {
+      axios({
+        url: "/api-token-auth/",
+        method: "post",
+        data: {
+          email: this.$route.params.email,
+          password: this.$route.params.email
+        }
+      }).then(res => {
+        console.log(res);
+        localStorage.setItem("token", "PAPER " + res.data.token);
+        window.location.reload();
+      });
+    }
+  }
+};
+</script>
+<style lang=''>
+</style>
